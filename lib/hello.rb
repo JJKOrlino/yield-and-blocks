e4ee9fc3ad975449076db4d82324d7384ab19b66
JJKOrlino <jjkorlino80@yahoo.com>
2020-11-15 14:25:34 -0600

    commit tree a6d8eea7fd6526190d672ec371d7463612d878cc

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,23 @@
-def hello_t
+def hello_t(array)
+    if block_given?
+        i = 0
+
+        while i < array.length #while i is less than index, continues
+            yield(array[i]) #block specific
+            i = i + 1
+        end
+
+        array #return changed values to array ??
+    else
+        puts "Hey! No block was given!"
+    end
 
 end
 
-# call your method here!
+
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+    if name.start_with?("T") #will only give names starting with "T"
+      puts "Hi, #{name}"
+    end
+  end 
 
